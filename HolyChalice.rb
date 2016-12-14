d3959224d3b741e413eeb2a94071f91dd1cea527
723 <rb.natsumi@gmail.com>
2016-12-14 11:27:20 +0900

    やっぱ定数に

--- HolyChalice.rb
+++ HolyChalice.rb
@@ -1,27 +1,34 @@
 #!/usr/bin/env ruby 
 
 class HolyChalice
+  FRAST_LANCER = [0,0]
+  FRAST_SABER = [0,7]
+  FRAST_BERSERKER = [7,0]
+  SECOND_LANCER = [14,14]
+  SECOND_SABER = [14,7]
+  SECOND_BERSERKER = [7,14]
+
   def initialize
     start_info = gets.to_i
 
     @field_status = Array.new()
     case start_info
     when 0
-      @ally_lancer = [0,0]
-      @ally_saber = [0,7]
-      @ally_berserker = [7,0]
+      @ally_lancer = FRAST_LANCER
+      @ally_saber = FRAST_SABER
+      @ally_berserker = FRAST_BERSERKER
 
-      @enemy_lancer = [14,14]
-      @enemy_saber = [14,7]
-      @enemy_berserker = [7,14]
+      @enemy_lancer = SECOND_LANCER
+      @enemy_saber = SECOND_SABER
+      @enemy_berserker = SECOND_BERSERKER
     when 1
-      @ally_lancer = [14,14]
-      @ally_saber = [14,7]
-      @ally_berserker = [7,17]
+      @ally_lancer = SECOND_LANCER
+      @ally_saber = SECOND_SABER
+      @ally_berserker = SECOND_BERSERKER
 
-      @enemy_lancer = [0,0]
-      @enemy_saber = [0,7]
-      @enemy_berserker = [7,0]
+      @enemy_lancer = SECOND_LANCER
+      @enemy_saber = SECOND_SABER
+      @enemy_berserker = SECOND_BERSERKER
     end
     puts 0
   end
