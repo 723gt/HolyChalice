f4b849d7f6cb39585c362792f9c56619e5e3b962
723 <rb.natsumi@gmail.com>
2016-12-16 14:36:25 +0900

    インデント修正

--- HolyChalice.rb
+++ HolyChalice.rb
@@ -13,6 +13,7 @@ class HolyChalice
     
     @turn_status = nil
     @field_status = Array.new
+
     case start_info
     when FRAST_PLAY
       @ally_lancer = FRAST_LANCER
@@ -36,13 +37,16 @@ class HolyChalice
 
   def turn_info
    fieldcnt = ZERO_INIT
+
    INFO_NUM.times do |i|
      data = get_data().chomp.gsub(" ","")
      if i == TURN_NUM
        @turn_status = data
      elsif i <= ENBE_NUM + COUNT_ADD
        $minusmv = ZERO_INIT
+
        datasize = data.length
+
        for j in INFO_START ... datasize do 
          case i
          when ALLA_NUM + PLAYER_SHIFT
@@ -64,9 +68,11 @@ class HolyChalice
        end
      else
        tmparray = Array.new
+
        for x in FIELD_MIN ... FIELD_MAX
          tmparray[x] = data[x].to_i
-       end  
+       end
+
        @field_status[fieldcnt] = tmparray
        fieldcnt += COUNT_ADD  
      end
