9c5e5ac5d199f61036a872c3234bc54a02baf8e3
723 <rb.natsumi@gmail.com>
2016-12-16 13:48:46 +0900

    get_xxxのメソッドをモジュール化

--- HolyChalice.rb
+++ HolyChalice.rb
@@ -1,9 +1,11 @@
 #!/usr/bin/env ruby 
 require_relative "lib/constmod"
+require_relative "lib/getmethod"
 
 class HolyChalice
   $minusmv = nil
   include Constmod
+  include Getmethod
   def initialize
     start_info = gets.to_i
     
@@ -67,42 +69,6 @@ class HolyChalice
        fieldcnt += 1  
      end
    end  
-  end
-
-  def get_data
-    return gets.to_s
-  end
-
-  def get_myberserker 
-    return @ally_berserker
-  end
-
-  def get_mylancer
-    return @ally_lancer
-  end
-
-  def get_mysaber
-    return @ally_saber
-  end
-
-  def get_eneberserker
-    return @enemy_berserker
-  end
-
-  def get_enelancer
-    return @enemy_lancer
-  end
-
-  def get_enesaber
-    return @enemy_saber
-  end
-
-  def get_field
-    return @field_status
-  end
-
-  def get_turn
-    return @turn_status
   end
 
   private 
